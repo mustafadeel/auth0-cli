203bf1a27769f5ea947e95c18c1b46d12f205d0d
Cyril David <cyril.david@auth0.com>
2021-05-28 10:14:41 -0700

    Fix the tenant and trigger translations

--- internal/cli/logger0.go
+++ internal/cli/logger0.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/url"
+	"strings"
 	"time"
 
 	"github.com/auth0/auth0-cli/internal/ansi"
@@ -32,17 +33,19 @@ func logger0Stream(ctx context.Context, tenant, trigger, actionID string) error 
 		return err
 	}
 
-	stream, err := client.CreateSession(ctx, &controlv1.CreateSessionRequest{
+	req := &controlv1.CreateSessionRequest{
 		Type:   logger0.LogRecord_TYPE_ACTIONS,
-		Tenant: tenant,
+		Tenant: translateTenant(tenant),
 
 		// TODO(cyx): we should use constants exposed by logger0 here
 		// instead, or an enum in the proto layer.
 		Filters: map[string]string{
 			"action_id":    actionID,
-			"trigger_type": trigger,
+			"trigger_type": translateTrigger(trigger),
 		},
-	})
+	}
+
+	stream, err := client.CreateSession(ctx, req)
 	if err != nil {
 		return err
 	}
@@ -60,4 +63,17 @@ func logger0Stream(ctx context.Context, tenant, trigger, actionID string) error 
 func formatLogRecord(rec *logger0.LogRecord) {
 	msgs := bytes.Join(rec.Messages, []byte("\n"))
 	fmt.Printf("%s - %s\n", ansi.Yellow(time.Now().Format(time.RFC3339)), msgs)
+}
+
+// NOTE(cyx): this is only necessary because we're punching directly to actions
+// protocol. API2 in theory should do this for us (it already does this
+// translation).
+func translateTrigger(t string) string {
+	return strings.Replace(strings.ToUpper(t), "-", "_", -1)
+}
+
+// NOTE(cyx): should use a domain util helper
+func translateTenant(t string) string {
+	chunks := strings.Split(t, ".")
+	return chunks[0]
 }
